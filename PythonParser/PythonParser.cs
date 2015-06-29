23e29c998e0a10954313437dfc7cdd8c87699060
ashwinjv <hgwells2015@gmail.com>
2015-06-28 23:50:12 -0700

    Initial Commit

---
+++ PythonParser/PythonParser.cs
@@ -0,0 +1,30 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PythonParser
+{
+    public class PythonParser
+    {
+        public void MyParse()
+        {
+            string input = @"
+import os, sys
+def test():
+    a = 1";
+            AntlrInputStream stream = new AntlrInputStream(input);
+            ITokenSource lexer = new Python3Lexer(stream);
+            ITokenStream tokens = new CommonTokenStream(lexer);
+            Python3Parser parser = new Python3Parser(tokens);
+            parser.BuildParseTree = true;
+            KeyPrinter printer = new KeyPrinter();
+            IParseTree tree = parser.file_input();
+            ParseTreeWalker.Default.Walk(printer, tree);
+            //IParseTree tree = parser.StartRule(); 
+        }
+    }
+}
